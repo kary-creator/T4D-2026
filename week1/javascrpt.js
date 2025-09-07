7bdf583867091136a21d96f11edf61e0e05531e7
kary-creator <kariminancy097@gmail.com>
2025-09-07 18:31:20 +0530

    week1assignment

---
+++ week1/javascrpt.js
@@ -0,0 +1,11 @@
+let num1 = 4;
+let num2 = 7;
+let name = "Nancy";
+let result = num1 + num2;
+
+console.log( num1 + num2);
+console.log( num1 - num2);
+console.log( num1 * num2);
+console.log( num1 / num2);
+console.log("Hello " + name +"," + "the result of  num1 + num2 is" + result);
+console.log(`Hello ${name}, the result of ${num1} + ${num2} is ${result}.`);
